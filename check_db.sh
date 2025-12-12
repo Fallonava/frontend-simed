6e115002432f4ff420c699cb95cc6a63ac717137
FaishalFx <238270619+Fallonava@users.noreply.github.com>
2025-12-12 16:35:55 +0700

    chore: Remove various development and database utility scripts, logs, and a database dump.

--- check_db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo -u postgres psql -d simed -c 'SELECT count(*) FROM "User"'
-sudo -u postgres psql -d simed -c 'SELECT count(*) FROM "Patient"'
--- diagnose_login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "=== USERS ==="
-sudo -u postgres psql -d simed -c 'SELECT username, role FROM "User";'
-
-echo "=== LOGIN TEST ==="
-curl -v -X POST -H 'Content-Type: application/json' -d '{"username":"admin","password":"admin123"}' http://127.0.0.1:3000/api/auth/login
